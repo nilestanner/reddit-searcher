d9035ae1843418f2695921e5e4e753b3c8498d81
Niles Tanner <niles.tanner@gmail.com>
2019-02-24 14:09:56 -0600

    added save to db option

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser= require('body-parser');
 const PORT = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}))
@@ -28,15 +29,21 @@ const textService = freeTextAPI({
     }
 });
 
-app.listen(PORT, () => console.log(`Reddit Scanner listening on port ${PORT}!`));
+MongoClient.connect(process.env.MONGODB_URI, async (err, client) => {
+    global.redditDB = client.db(process.env.MONGODBNAME);
+    app.listen(PORT, () => console.log(`Reddit Scanner listening on port ${PORT}!`));
+});
+
+
 
 app.get('/search', (req, res) => {
     request.get(process.env.SEARCHURL, (err, res, body) => {
         const json = JSON.parse(body);
         if (json.data.dist > 0) {
-            json.data.children.forEach((child) => {
+            json.data.children.forEach((child, i) => {
                 const message = `${child.data.url}`;
                 sendTextUpdate(message);
+                saveToDb(child)
             });
         }
     });
@@ -46,6 +53,11 @@ app.get('/search', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Hello, you have found me');
 });
+
+const saveToDb = (post) => {
+    const search = {created_utc: post.created_utc};
+    global.redditDB.collection('cvpposts').updateOne(search, {$set: post}, {'upsert':true})
+}
 
 const sendTextUpdate = (message) => {
     textService.sendText({
