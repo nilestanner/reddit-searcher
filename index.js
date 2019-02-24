1a9b65f0d381fba55644ae2ec4a1511e3b753c84
Niles Tanner <niles.tanner@gmail.com>
2019-02-24 16:03:40 -0600

    checking now for price changes too

--- index.js
+++ index.js
@@ -34,23 +34,19 @@ MongoClient.connect(process.env.MONGODB_URI, async (err, client) => {
     app.listen(PORT, () => console.log(`Reddit Scanner listening on port ${PORT}!`));
 });
 
-app.get('/search', (req, res) => {
-    request.get(process.env.SEARCHURL, (err, res, body) => {
-        const json = JSON.parse(body);
-        if (json.data.dist > 0) {
-            checkPosts(json.data.children);
-        }
-    });
+app.get('/search', async (req, res) => {
     res.send('Job started');
+    await searchWithKeyword('cvp', 'cvpposts');
+    await searchWithKeyword('price changes', 'pcposts');
 });
 
 app.get('/', (req, res) => {
     res.send('Hello, you have found me');
 });
 
-const saveToDb = async (post) => {
+const saveToDb = async (post, collection) => {
     const search = {created_utc: post.created_utc};
-    await global.redditDB.collection('cvpposts').updateOne(search, {$set: post}, {'upsert':true})
+    await global.redditDB.collection(collection).updateOne(search, {$set: post}, {'upsert':true})
 }
 
 const sendTextUpdate = (message) => {
@@ -61,23 +57,36 @@ const sendTextUpdate = (message) => {
     });
 }
 
-const checkPosts = async (posts) => {
+const checkPosts = async (posts, collection) => {
     for (post of posts) {
         post = post.data;
-        const notFound = await findPost(post);
+        const notFound = await findPost(post, collection);
         if (notFound) {
             const message = `${post.url}`;
             sendTextUpdate(message);
-            await saveToDb(post);
+            await saveToDb(post, collection);
         }
     }
 }
 
-const findPost = async (post) => {
+const findPost = async (post, collection) => {
     return new Promise((resolve, reject) => {
-        global.redditDB.collection('cvpposts').findOne({created_utc: post.created_utc}, (err, result) => {
+        global.redditDB.collection(collection).findOne({created_utc: post.created_utc}, (err, result) => {
             resolve(!result)
         });
     });
+}
+
+const searchWithKeyword = async (searchString, collection) => {
+    return new Promise((resolve, reject) => {
+        request.get(`${process.env.SEARCHURL}&q=${searchString}`, async (err, res, body) => {
+            const json = JSON.parse(body);
+            if (json.data.dist > 0) {
+                await checkPosts(json.data.children, collection);
+            }
+            resolve();
+        });
+    });
+    
 }
 
